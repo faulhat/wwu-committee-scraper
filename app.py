a52494985014ca7e28ad35d75efedee803d2e9ee
Silverson-EthanG <griffi38@wwu.edu>
2025-03-04 11:30:34 -0800

    added maybe pathway for giving json files.

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, render_template
+import json
+import os
+from flask import Flask, render_template, jsonify, abort, request
 
 # Getting the app started
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+
+def folder_exists(path):
+    return os.path.isdir(path)
+
+# Display static HTML.
+# @app.route('/')
+# def index():
+#     return render_template("index.html")
+
+# Search for JSON data.
+@app.route('/data/<path:subpath>')
+def load_json_file(filename):
+    if not folder_exists('/data'):
+        abort(404)
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return jsonify(data)
+
 
 
 if __name__ == "__main__":
