e7a8da56a7aaf8516b73c5a037c0fa2c02b16bc8
Tom Faulhaber <faulhat@wwu.edu>
2025-04-29 09:23:03 -0700

    Added new function to search a string for arbitrary terms.

--- gosearch/terms.go
+++ gosearch/terms.go
@@ -1,5 +1,15 @@
 package main
 
+/* Return a trie mapping each term in w to its number of appearances in s */
+func Search(w []string, s string) *TrieNode {
+	t := TrieFromList(w)
+	for i := 0; i < len(s); i++ {
+		t.Prefix(s, i)
+	}
+
+	return t
+}
+
 /* Increment all entries in n which match s starting from position c. */
 func (n *TrieNode) Prefix(s string, c int) {
 	if n.term {
@@ -7,6 +17,6 @@ func (n *TrieNode) Prefix(s string, c int) {
 	}
 
 	if c < len(s) && n.children[s[c]] != nil {
-		n.children[s[c]].Prefix(s, c + 1)
+		n.children[s[c]].Prefix(s, c+1)
 	}
 }
--- gosearch/terms_test.go
+++ gosearch/terms_test.go
@@ -2,17 +2,18 @@ package main
 
 import "testing"
 
-func TestPrefix(t *testing.T) {
-	words := map[string]int{
-		"hills": 1,
-		"hill": 3,
-		"hillsborough": 1,
-		"hampton": 1,
-		"hillyside": 2,
-		"hillysides": 1,
-	}
+var words = map[string]int{
+	"hills":        1,
+	"hill":         3,
+	"hillsborough": 1,
+	"hampton":      1,
+	"hillyside":    2,
+	"hillysides":   1,
+}
 
-	nonsense := "hillsborough hampton hillyside hillysides"
+var nonsense = "hillsborough hampton hillyside hillysides"
+
+func TestPrefix(t *testing.T) {
 	trie := new(TrieNode)
 	for word := range words {
 		trie.Set(word, 0)
@@ -23,6 +24,23 @@ func TestPrefix(t *testing.T) {
 	trie.Prefix(nonsense, 21)
 	trie.Prefix(nonsense, 31)
 
+	for word, n := range words {
+		v, ok := trie.Get(word)
+		if !ok {
+			t.Errorf("Trie didn't contain expected word %v.", word)
+		} else if v != n {
+			t.Errorf("Trie didn't contain expected value for word %v. Got %v", word, v)
+		}
+	}
+}
+
+func TestSearch(t *testing.T) {
+	terms := make([]string, 0, len(words))
+	for k := range words {
+		terms = append(terms, k)
+	}
+
+	trie := Search(terms, nonsense)
 	for word, n := range words {
 		v, ok := trie.Get(word)
 		if !ok {
