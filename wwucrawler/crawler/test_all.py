949d93998b9015dc77feef11b93defd3cf709aeb
Tom Faulhaber <faulhat@wwu.edu>
2025-06-06 19:09:56 -0700

    Squashed commit of the following:
    commit 9bccf4b1be821cb49dc607923aa4e8d090449778
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Fri Jun 6 18:58:43 2025 -0700
    
        Added tests for tiered search
    
    commit 66d0b0f69e33a9803aaaedf94a907fa4cc0f67e0
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Fri Jun 6 17:16:00 2025 -0700
    
        Added test for the basic search function
    
    commit 66c048bb713cad823acbc59e9060c0439bbfbad7
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Fri Jun 6 17:03:51 2025 -0700
    
        Added test data
    
    commit 161185207bf2c763d8157a9d2a15c13ec106ceb6
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Fri Jun 6 16:56:36 2025 -0700
    
        Wrote passing database test
    
    commit 160eb54c595875f623df92e4534abae6f1778b9a
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Fri Jun 6 16:33:22 2025 -0700
    
        Added pytest

---
+++ wwucrawler/crawler/test_all.py
@@ -0,0 +1,98 @@
+import pytest
+
+from bs4 import BeautifulSoup
+
+from pages_db import PagesDB
+from search import SearchRes, search, tiered_search
+
+# Mock database
+test_db = PagesDB(":memory:")
+test_db.setup()
+
+
+def test_add_page():
+    assert test_db.count_pages() == 0
+
+    search_null = SearchRes({}, -1, -1, 0)
+    test_db.add_page("helloworld.com", "Hello World", search_null, 0, "")
+    assert test_db.count_pages() == 1
+
+    cur = test_db.con.cursor()
+    cur.execute("SELECT url, title, score FROM pages")
+    rows = cur.fetchall()
+
+    assert len(rows) == 1
+    assert rows[0][0] == "helloworld.com"
+    assert rows[0][1] == "Hello World"
+    assert rows[0][2] == 0
+
+
+def test_search():
+    expected_appearances = {
+        "vast": 1,
+        "inhabit": 1,
+        "portion": 1,
+        "the": 5,
+    }
+
+    with open("testdata/sample.html", "r") as f:
+        text = f.read()
+        soup = BeautifulSoup(text, "html.parser")
+        res = search(soup.text, expected_appearances.keys())
+        for term, num in expected_appearances.items():
+            assert term in res.appearances
+            assert res.appearances[term] == num
+
+        assert res.total == 8
+
+
+def test_tiered_search():
+    keywords = [["vast", "inhabit"], ["portion"], ["the"]]
+    expected_appearances = {
+        "vast": 1,
+        "inhabit": 1,
+        "portion": 1,
+        "the": 5,
+    }
+
+    with open("testdata/sample.html", "r") as f:
+        text = f.read()
+        soup = BeautifulSoup(text, "html.parser")
+        res = tiered_search(soup.text, keywords)
+        for term, num in expected_appearances.items():
+            assert term in res.appearances
+            assert res.appearances[term] == num
+
+        assert res.total == 13
+
+
+def test_summarize():
+    keywords = [["vast", "inhabit"], ["portion"], ["the"]]
+    expected_appearances = {
+        "vast": 1,
+        "inhabit": 1,
+        "portion": 1,
+        "the": 5,
+    }
+
+    fname = "testdata/sample.html"
+    with open(fname, "r") as f:
+        soup = BeautifulSoup(f.read(), "html.parser")
+        title = soup.title.string
+        text = soup.text
+        res = tiered_search(text, keywords)
+        test_db.add_page(fname, title, res, res.total, text)
+
+        cur = test_db.con.cursor()
+        cur.execute(
+            "SELECT summary_before, summary_keyword, summary_after FROM pages WHERE url = ?",
+            (fname,),
+        )
+        rows = cur.fetchall()
+        summary_before, summary_keyword, summary_after = rows[0]
+
+        assert summary_keyword == "vast"
+        assert summary_before.startswith("...")
+        assert summary_after.endswith("...")
+        assert summary_before[3:] == text[res.first - 10 : res.first]
+        assert summary_after[:-3] == text[res.end : res.end + 100]
