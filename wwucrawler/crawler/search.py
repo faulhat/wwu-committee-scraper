6b9342e1d4976903b36615dc0949bc0a27674ead
Tom Faulhaber <faulhat@wwu.edu>
2025-11-25 16:36:07 -0800

    Squashed commit of the following:
    commit a9e190dc0b6e86e2332fa3b8555a033df4f82166
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Tue Nov 25 16:27:58 2025 -0800
    
        Removed old search
    
    commit cb8ac893d9fbd6b4d0bdd0fd8bf6bbf11cc904bf
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Tue Nov 25 16:27:16 2025 -0800
    
        Tiered search now reuses tries instead of making new ones on every call.
    
    commit 5a38acbb0f142f3815d6bba62e5ebb6656507ee7
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Tue Nov 25 16:20:35 2025 -0800
    
        Got trie search working
    
    commit 43b07d14b23362ca66509036cb7e094c76731329
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Wed Nov 12 14:32:26 2025 -0800
    
        Continued work on trie class
    
    commit 76e03464cb633bd65e8caca75d488eedd6db2ca9
    Author: Tom Faulhaber <faulhat@wwu.edu>
    Date:   Tue Nov 4 21:31:33 2025 -0800
    
        Worked on trie class for optimized search

--- wwucrawler/crawler/search.py
+++ wwucrawler/crawler/search.py
@@ -1,4 +1,5 @@
 import re
+from queue import Queue
 
 
 # Result of searching a document
@@ -15,12 +16,11 @@ class SearchRes:
 # Search with keywords of varying priority
 #  Keywords on the first priority tier out of n will count for n points each,
 #  those on the second tier count for n-1, and so on.
-def tiered_search(text, keyword_list):
-    score = 0
-    result = search(text, keyword_list[0])
-    result.total *= len(keyword_list)
-    for i, tier in enumerate(keyword_list[1:]):
-        t_res = search(text, tier)
+def tiered_search(text, keyword_tries):
+    result = keyword_tries[0].search(text)
+    result.total *= len(keyword_tries)
+    for i, tier in enumerate(keyword_tries[1:]):
+        t_res = tier.search(text)
         for term, num in t_res.appearances.items():
             if term in result.appearances:
                 result.appearances[term] += num
@@ -31,29 +31,99 @@ def tiered_search(text, keyword_list):
             result.first = t_res.first
             result.end = t_res.end
 
-        result.total += (len(keyword_list) - i - 1) * t_res.total
+        result.total += (len(keyword_tries) - i - 1) * t_res.total
 
     return result
 
 
-# Search a document for given keywords
-# TODO:  Make this use a trie
-def search(text, terms):
-    appearances = {}
-    first = -1
-    end = -1
-    total = 0
+class TrieNode:
+    def __init__(self, terminal=False, term=None, occurrences=0, node=None):
+        self.terminal = terminal
+        self.term = term
+        self.node = node
 
-    for term in terms:
-        pattern = re.compile(f"(?<!\w){term}(?!\w)")
-        cur = 0
-        while match := pattern.search(text, cur):
-            appearances[term] = 1 + appearances.get(term, 0)
-            if first < 0 or match.end() < first:
-                first = match.start()
-                end = match.end()
 
-            cur = match.end()
-            total += 1
+class Trie:
+    def __init__(self, terms):
+        self.terms = terms
+        self.root = [TrieNode() for i in range(256)]
+        for term in terms:
+            self.add_term(term)
 
-    return SearchRes(appearances, first, end, total)
+    def print_all(self):
+        q = Queue()
+        q.put(("", self.root))
+        while not q.empty():
+            s, n = q.get()
+        for i, b in enumerate(n):
+            t = s + chr(i)
+            if b.node is not None:
+                q.put((t, b.node))
+
+            if b.terminal:
+                print(f"{t}, {b.term}")
+
+    def add_term(self, term):
+        for c in term:
+            if ord(c) > 255:
+                raise Exception("Tries can only use ASCII terms.")
+
+        n = self.root
+        for i, c in enumerate(term):
+            b = ord(c)
+            if i == len(term) - 1:
+                n[b].terminal = True
+                n[b].term = term
+                return n[b]
+            else:
+                if n[b].node is None:
+                    n[b].node = [TrieNode() for i in range(256)]
+
+                n = n[b].node
+
+    def get_node(self, term):
+        n = self.root
+        for i, c in enumerate(term):
+            b = ord(c)
+            if b > 255:
+                return None
+
+            if i == len(term) - 1:
+                return n[b] if n[b].terminal else None
+
+            if n[b].node is None:
+                return None
+
+            n = n[b].node
+
+    def has_term(self, term):
+        return self.get_node(term) is not None
+
+    def search(self, text):
+        appearances = {}
+        first = -1
+        end = -1
+        total = 0
+
+        i = 0
+        for i in range(len(text)):
+            n = self.root
+            for j, c in enumerate(text[i:]):
+                b = ord(c)
+                if b > 255:
+                    break
+
+                if n[b].terminal:
+                    if first < 0:
+                        first = i
+                        end = i + j + 1
+
+                    appearances[n[b].term] = 1 + appearances.get(n[b].term, 0)
+                    total += 1
+
+                if n[b].node is None:
+                    break
+
+                n = n[b].node
+
+        return SearchRes(appearances, first, end, total)
